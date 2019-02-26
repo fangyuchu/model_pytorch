50894ed16517135380dd90388fa5e9efa597c34b
victorfang <569160112@qq.com>
2019-02-26 19:52:06 +0800

    将prune改进为根据卷基层的数量剪枝

--- prune.py
+++ prune.py
@@ -3,27 +3,32 @@ import numpy as np
 import vgg
 
 
-def replace_layers(model, i, indexes, layers):
-    if i in indexes:
-        return layers[indexes.index(i)]
-    return model[i]
+def replace_layers(module,old_mod,new_mod):
+    for i in range(len(old_mod)):
+        if module is old_mod[i]:
+            return new_mod[i]
+    return module
+
+
 
 
 
 def prune_conv_layer(model, layer_index, filter_index):
-    ''' layer_index:要删的是哪一层中的filter，这个layer_index指向的必须要是conv
+    ''' layer_index:要删的卷基层的索引
         filter_index:要删layer_index层中的哪个filter
     '''
-    _, conv = list(model.features._modules.items())[layer_index]            #获得要删filter的那层conv
-    next_conv = None
-    offset = 1
-
-    while layer_index + offset < len(model.features._modules.items()):
-        res = list(model.features._modules.items())[layer_index + offset]
-        if isinstance(res[1], torch.nn.modules.conv.Conv2d):                #获得要删的下一层的conv
-            next_name, next_conv = res
-            break
-        offset = offset + 1
+    moduleList=list(model.features._modules.items())
+    conv=None                                                               #获取要删filter的那层conv
+    next_conv=None                                                          #如果有的话：获取要删的那层后一层的conv，用于删除对应通道
+    i=0
+    for mod in moduleList:
+        if isinstance(mod[1],torch.nn.modules.conv.Conv2d):
+            i+=1
+            if conv is not None:                                            #要删的filter后一层的conv
+                next_conv=mod[1]
+                break
+            if i==layer_index:                                              #要删filter的conv
+                conv=mod[1]
 
     new_conv = torch.nn.Conv2d(                                             #创建新的conv替代要删filter的conv
                                 in_channels=conv.in_channels,
@@ -35,19 +40,16 @@ def prune_conv_layer(model, layer_index, filter_index):
                                 groups=conv.groups,
                                 bias=(conv.bias is not None))
 
-
     old_weights = conv.weight.data.cpu().numpy()
     new_weights = new_conv.weight.data.cpu().numpy()
-
+                                                                                    #删除卷积核
     new_weights[: filter_index, :, :, :] = old_weights[: filter_index, :, :, :]     #将索引前的filer复制到新conv中
     new_weights[filter_index:, :, :, :] = old_weights[filter_index + 1:, :, :, :]   #将索引后一个直至最后的所有filter复制到新conv中
     new_conv.weight.data = torch.from_numpy(new_weights)
     if torch.cuda.is_available():
         new_conv.weight.data=new_conv.weight.data.cuda()
-
     if conv.bias is not None:
         bias_numpy = conv.bias.data.cpu().numpy()
-
         bias = np.zeros(shape=(bias_numpy.shape[0] - 1), dtype=np.float32)
         bias[:filter_index] = bias_numpy[:filter_index]
         bias[filter_index:] = bias_numpy[filter_index + 1:]
@@ -69,7 +71,6 @@ def prune_conv_layer(model, layer_index, filter_index):
 
         old_weights = next_conv.weight.data.cpu().numpy()
         new_weights = next_new_conv.weight.data.cpu().numpy()
-
         new_weights[:, : filter_index, :, :] = old_weights[:, : filter_index, :, :]
         new_weights[:, filter_index:, :, :] = old_weights[:, filter_index + 1:, :, :]
         next_new_conv.weight.data = torch.from_numpy(new_weights).cpu()
@@ -78,24 +79,20 @@ def prune_conv_layer(model, layer_index, filter_index):
         if next_conv.bias is not None:
             next_new_conv.bias.data = next_conv.bias.data
 
-    if not next_conv is None:                                                                     #根据新定义好的module重新定义sequential
-        features = torch.nn.Sequential(
-            *(replace_layers(model.features, i, [layer_index, layer_index + offset], [new_conv, next_new_conv]) for i, _ in enumerate(model.features)))
-        del model.features
-        del conv
+        model.features=torch.nn.Sequential(                                               #生成替换为新conv的feature
+            *(replace_layers(mod,[conv,next_conv],[new_conv,next_new_conv]) for mod in model.features))
 
-        model.features = features
-        #todo：为什么要把relu改成batchnorm？
-        if torch.cuda.is_available():
-            model.features[layer_index + 1] = torch.nn.BatchNorm2d(model.features[layer_index].out_channels).cuda()
-        else:
-            model.features[layer_index + 1] = torch.nn.BatchNorm2d(model.features[layer_index].out_channels)
+        #为什么要把relu改成batchnorm？
+        # if torch.cuda.is_available():
+        #     model.features[layer_index + 1] = torch.nn.BatchNorm2d(model.features[layer_index].out_channels).cuda()
+        # else:
+        #     model.features[layer_index + 1] = torch.nn.BatchNorm2d(model.features[layer_index].out_channels)
 
     else:
         # Prunning the last conv layer. This affects the first linear layer of the classifier.
         model.features = torch.nn.Sequential(
-            *(replace_layers(model.features, i, [layer_index], [new_conv]) for i, _ in enumerate(model.features)))
-        model.features[layer_index + 1] = torch.nn.BatchNorm2d(model.features[layer_index].out_channels)
+            *(replace_layers(mod, [conv], [new_conv]) for mod in model.features))
+        # model.features[layer_index + 1] = torch.nn.BatchNorm2d(model.features[layer_index].out_channels)
 
         layer_index = 0
         old_linear_layer = None
@@ -127,13 +124,8 @@ def prune_conv_layer(model, layer_index, filter_index):
         if torch.cuda.is_available():
             new_linear_layer.weight.data=new_linear_layer.weight.data.cuda()
 
-        classifier = torch.nn.Sequential(
-            *(replace_layers(model.classifier, i, [layer_index], [new_linear_layer]) for i, _ in enumerate(model.classifier)))
-
-        del model.classifier
-        del next_conv
-        del conv
-        model.classifier = classifier
+        model.classifier = torch.nn.Sequential(
+            *(replace_layers(mod, [old_linear_layer], [new_linear_layer]) for mod in model.classifier))
 
     return model
 
@@ -148,22 +140,18 @@ def select_and_prune_filter(model,layer_index,num_to_prune,ord):
     '''
     if ord!=1 and ord !=2:
         raise TypeError('unsupported type of norm')
-    _, conv = list(model.features._modules.items())[layer_index]            #get conv module
-    weights = conv.weight.data.cpu().numpy()                                #get weight of all filters
-
-    filter_norm=np.linalg.norm(weights,ord=ord,axis=(2,3))                            #compute filters' norm
-    if ord==1:
-        filter_norm=np.sum(filter_norm,axis=1)
-    elif ord==2:
-        filter_norm=np.square(filter_norm)
-        filter_norm=np.sum(filter_norm,axis=1)
-
-    filter_norm_sorted_index=np.argsort(filter_norm)
-
-    #model=prune_conv_layer(model,layer_index,filter_norm_sorted_index[0:num_to_prune])
-    #todo: prune_conv_layer不支持一次删多个卷积核
-    model=prune_conv_layer(model,layer_index,filter_norm_sorted_index[0])
-
+    while num_to_prune>0:                                                   #todo:同时删多个卷积核可以写的更精简，暂时懒得改了
+        _, conv = list(model.features._modules.items())[layer_index]  # get conv module
+        weights = conv.weight.data.cpu().numpy()  # get weight of all filters
+        num_to_prune-=1
+        filter_norm=np.linalg.norm(weights,ord=ord,axis=(2,3))                            #compute filters' norm
+        if ord==1:
+            filter_norm=np.sum(filter_norm,axis=1)
+        elif ord==2:
+            filter_norm=np.square(filter_norm)
+            filter_norm=np.sum(filter_norm,axis=1)
+        filter_min_norm_index=np.argmin(filter_norm)
+        model=prune_conv_layer(model,layer_index,filter_min_norm_index)
 
     return model
 
@@ -171,5 +159,5 @@ def select_and_prune_filter(model,layer_index,num_to_prune,ord):
 
 if __name__ == "__main__":
     model= vgg.vgg11(False)
-    select_and_prune_filter(model,layer_index=3,num_to_prune=1,ord=2)
+    select_and_prune_filter(model,layer_index=8,num_to_prune=1,ord=2)
     prune_conv_layer(model,layer_index=3,filter_index=1)
