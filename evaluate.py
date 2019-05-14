ad055af5d4add739262bb8c7c4121cff85fcf635
victorfang <569160112@qq.com>
2019-05-14 16:35:18 +0800

    fix one bug in evaluation

--- evaluate.py
+++ evaluate.py
@@ -51,7 +51,7 @@ def accuracy(output, target, topk=(1,)):
     res = []
     for k in topk:
         correct_k = correct[:k].view(-1).float().sum(0)
-        res.append(correct_k.mul_(100.0 / batch_size))
+        res.append(correct_k.mul_(1 / batch_size))                  #each item is one k_accuracy 
     return res
 
 
