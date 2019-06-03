5979e7a6f1a163ac8897c1cd446da621157b1339
VictorFang <nb960801>
2019-06-03 21:07:54 +0800

    beta

--- test.py
+++ test.py
@@ -11,46 +11,26 @@ import evaluate
 import numpy as np
 import data_loader
 
-# checkpoint=torch.load('/home/victorfang/Desktop/pytorch_model/vgg16_bn_cifar10_dead_neural_pruned/checkpoint/sample_num=9950000,accuracy=0.930.tar')
-# net=checkpoint['net']
-# net.load_state_dict(checkpoint['state_dict'])
-# accuracy=checkpoint['highest_accuracy']
-# print(accuracy)
-# measure_flops.measure_model(model=net,dataset_name='cifar10')
+neural_dead_times=8000
+filter_dead_ratio=0.85
 
-
-
-
-
-neural_dead_times=40000
-filter_dead_ratio=0.9
-
-checkpoint = torch.load('/home/victorfang/Desktop/vgg19_imagenet_deadReLU.tar')
-# # checkpoint = torch.load('/home/victorfang/Desktop/pytorch_model/test5/checkpoint/sample_num=7800000,accuracy=0.911.tar')
-#
-# net = checkpoint['net']
-# net.load_state_dict(checkpoint['state_dict'])
-# print(checkpoint['highest_accuracy'])
-# measure_flops.measure_model(model=net,dataset_name='cifar10')
-# relu_list,neural_list=evaluate.check_ReLU_alive(net=net,
-#                           data_loader=data_loader.create_validation_loader(batch_size=1024,num_workers=6,dataset_name='cifar10'),
-#                           neural_dead_times=neural_dead_times)
-
+checkpoint=torch.load('round 1.tar')
 net=checkpoint['net']
-relu_list=checkpoint['relu_list']
+net.load_state_dict(checkpoint['state_dict'])
 neural_list=checkpoint['neural_list']
+relu_list=checkpoint['relu_list']
 
-evaluate.cal_dead_neural_rate(neural_dead_times=neural_dead_times,neural_list_temp=neural_list)
 num_conv = 0  # num of conv layers in the net
-filter_num_lower_bound = list()
-filter_num = list()
+filter_num=list()
+filters=list()
 for mod in net.features:
     if isinstance(mod, torch.nn.modules.conv.Conv2d):
         num_conv += 1
-        filter_num_lower_bound.append(int(mod.out_channels * 0.1))
         filter_num.append(mod.out_channels)
+        filters.append(mod)
 
-total_filters=np.sum(filter_num)
+dead_filter=np.empty(shape=[0,3,3,3])
+living_filter=np.empty(shape=[0,3,3,3])
 
 for i in range(num_conv):
     for relu_key in list(neural_list.keys()):
@@ -64,14 +44,11 @@ for i in range(num_conv):
             dead_relu_list[dead_relu_list>=neural_dead_times]=1
             dead_relu_list=np.sum(dead_relu_list,axis=(1,2))            #count the number of dead neural for one filter
             dead_filter_index=np.where(dead_relu_list>neural_num*filter_dead_ratio)[0].tolist()
-            #ensure the lower bound of filter number
-            if filter_num[i]-len(dead_filter_index)<filter_num_lower_bound[i]:
-                dead_filter_index=dead_filter_index[:filter_num[i]-filter_num_lower_bound[i]]
-            filter_num[i]=filter_num[i]-len(dead_filter_index)
-
-            print('layer {}: remain {} filters, prune {} filters.'.format(i, filter_num[i],
-                                                                          len(dead_filter_index)))
-total_filters_after_pruned=np.sum(filter_num)
+            living_filter_index=[i for i in range(filter_num[i]) if i not in dead_filter_index]
+            dead_filter=np.append(dead_filter,filters[i].weight.data.cpu().numpy()[dead_filter_index],axis=0)
+            living_filter=np.append(living_filter,filters[i].weight.data.cpu().numpy()[living_filter_index],axis=0)
+            print()
 
 
-print('before:{},after:{},prune{}'.format(total_filters,total_filters_after_pruned,1-total_filters_after_pruned/total_filters))
+
+
