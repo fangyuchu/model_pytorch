9d582755b335afc0ab42c128eb21359398b7d8d3
fang <569160112@qq.com>
2019-11-17 23:43:31 +0800

    add code for weighted channel

--- test.py
+++ test.py
@@ -21,39 +21,94 @@ import create_net
 import matplotlib.pyplot as plt
 import resnet_copied
 from torch import optim
+from torch.autograd import Variable
+import vgg_channel_weight
+
+# out=torch.Tensor(3,3)
+# a=Variable(torch.Tensor([1,2,4]),requires_grad=True)
+# b=Variable(torch.Tensor([3,4,7]),requires_grad=True)
+# e=Variable(torch.Tensor([3,4,7]),requires_grad=True)
+# sdf=Variable(torch.Tensor([2,5,8]),requires_grad=True)
+# out[0]=a
+# out[1]=b
+# out[2]=e
+# #
+# d=torch.sum(out)
+# d.backward()
+
+
+c=torch.load('/home/disk_new/model_saved/vgg16_bn_weighted_channel/checkpoint/flop=18923530,accuracy=0.70950.tar')
+
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-checkpoint = torch.load('/home/victorfang/PycharmProjects/model_pytorch/model_saved/vgg16bn_imagenet_prune_train/checkpoint/flop=12804890484,accuracy=0.90876.tar')
-# checkpoint=torch.load('/home/victorfang/PycharmProjects/model_pytorch/model_saved/vgg16bn_tinyimagenet_prune_train/checkpoint/flop=11394264872,accuracy=0.71270.tar')
-net=checkpoint['net']
-# net=resnet_copied.resnet56()
+# mod=vgg_channel_weight.conv2d_weighted_channel(in_channels=3,out_channels=64,kernel_size=3,padding=1).to(device)
+# print(isinstance(mod, nn.Conv2d))
+# dl=data_loader.create_validation_loader(batch_size=5,num_workers=4,dataset_name='cifar10')
+# for step, data in enumerate(dl, 0):
+#     images, labels = data
+#     images, labels = images.to(device), labels.to(device)
+#     c=mod(images)
+#     d=torch.sum(c)
+#     d.backward()
+#     print()
 
-#
-# # checkpoint = torch.load('./baseline/resnet56_cifar10,accuracy=0.93280.tar')
-# checkpoint=torch.load('./baseline/resnet56_cifar10,accuracy=0.94230.tar')
-# net = resnet_copied.resnet56().to(device)
 
-# # checkpoint=torch.load('/home/disk_new/model_saved/resnet56_cifar10_DeadNeural_realdata_good_baseline_过得去/代表/sample_num=13300000,accuracy=0.93610，flop=65931914.tar')
-# # net=checkpoint['net']
-#
-net.load_state_dict(checkpoint['state_dict'])
-print(checkpoint['highest_accuracy'])
+net=vgg_channel_weight.vgg16_bn(pretrained=False).to(device)
+net_normal=vgg.vgg16_bn(pretrained=False).to(device)
+
+
+net.classifier=nn.Sequential(
+            nn.Linear(512, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 10),
+        )
+
+
+net_normal.classifier=nn.Sequential(
+            nn.Linear(512, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 10),
+        )
+
+print("net_new have {} paramerters in total".format(sum(x.numel() for x in net.parameters())))
+print("net_normal have {} paramerters in total".format(sum(x.numel() for x in net_normal.parameters())))
+
+
+
+
+for m in net.modules():
+    if isinstance(m, nn.Linear):
+        nn.init.normal_(m.weight, 0, 0.01)
+        nn.init.constant_(m.bias, 0)
+
+net.to(device)
+
 train.train(net=net,
-            net_name='vgg16bn_imagenet_prune_train2',
-            dataset_name='imagenet',
-            test_net=True,
-            num_epochs=20,
+            net_name='vgg16_bn_weighted_channel',
+            dataset_name='cifar10',
+            learning_rate=0.1,
+            num_epochs=250,
+            batch_size=256,
             checkpoint_step=4000,
-            target_accuracy=0.9140571220008835,
-            batch_size=24,
-            top_acc=5,
-
-
-            optimizer=optim.SGD,
-            learning_rate=0.0001,
-            # weight_decay=0.0006,
-            momentum=0.9,
+            load_net=True,
+            test_net=True,
+            num_workers=4,
             learning_rate_decay=True,
-            learning_rate_decay_epoch=[5, 10, 15],
             learning_rate_decay_factor=0.1,
-            )
+            learning_rate_decay_epoch=[50,100,150,200])
+
+
+
+
+
+
+print()
---
+++ vgg_channel_weight.py
@@ -0,0 +1,255 @@
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
+import torch
+
+__all__ = [
+    'VGG', 'vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn', 'vgg16', 'vgg16_bn',
+    'vgg19_bn', 'vgg19',
+]
+
+
+model_urls = {
+    'vgg11': 'https://download.pytorch.org/models/vgg11-bbd30ac9.pth',
+    'vgg13': 'https://download.pytorch.org/models/vgg13-c768596a.pth',
+    'vgg16': 'https://download.pytorch.org/models/vgg16-397923af.pth',
+    'vgg19': 'https://download.pytorch.org/models/vgg19-dcbb9e9d.pth',
+    'vgg11_bn': 'https://download.pytorch.org/models/vgg11_bn-6002323d.pth',
+    'vgg13_bn': 'https://download.pytorch.org/models/vgg13_bn-abd245e5.pth',
+    'vgg16_bn': 'https://download.pytorch.org/models/vgg16_bn-6c64b313.pth',
+    'vgg19_bn': 'https://download.pytorch.org/models/vgg19_bn-c79401a0.pth',
+}
+
+# class add_channel_weight(nn.Module):
+#     def __init__(self,in_channels):
+#         super(add_channel_weight, self).__init__()
+#         self.weight = nn.Parameter(torch.ones((in_channels,1,1)))                           #weight of each channel initialized to 1
+#
+#     def forward(self,input):
+#         x=input*self.weight
+#         return x
+
+# from torch._six import container_abcs
+# from itertools import repeat
+# def _ntuple(n):
+#     def parse(x):
+#         if isinstance(x, container_abcs.Iterable):
+#             return x
+#         return tuple(repeat(x, n))
+#     return parse
+#
+# _single = _ntuple(1)
+# _pair = _ntuple(2)
+
+import torch.nn.functional as F
+
+class conv2d_weighted_channel(nn.modules.Conv2d):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+                 padding=0, dilation=1, groups=1, bias=True):
+        super(conv2d_weighted_channel, self).__init__(
+            in_channels, out_channels, kernel_size, stride, padding, dilation,
+            groups, bias)
+        self.channel_weight=nn.Parameter(torch.ones((out_channels,in_channels,1,1)),requires_grad=True)
+
+
+    def forward(self, input): 
+        weighted_weight=self.weight*self.channel_weight
+
+
+        out=F.conv2d(input, weighted_weight, self.bias, self.stride,
+                        self.padding, self.dilation, self.groups)
+
+        # try:
+        #     out_width=int((input.shape[2]+2*self.padding[0]-self.kernel_size[0])/self.stride[0])+1
+        #     out_height=int((input.shape[3]+2*self.padding[1]-self.kernel_size[1])/self.stride[1])+1
+        #     #起始203252224
+        #     # out=torch.Tensor(input.shape[0],self.out_channels,out_width,out_height).to(input.device)    #占用67108864，等于没有这些正常卷积时的开销
+        #     out=[]
+        #     # input_expand=torch.Tensor(self.out_channels,input.shape[0],input.shape[1],input.shape[2],input.shape[3]).to(input.device)
+        #     # for i in range(self.out_channels):
+        #     #     input_expand[i]=input*self.channel_weight[i]    #占用3145728
+        #
+        #     for i in range(self.out_channels):
+        #         input_i=input*self.channel_weight[i]    #占用3145728
+        #
+        #         c=self.weight[i].unsqueeze(0)
+        #         d=self.bias[i].unsqueeze(0)     #不占用
+        #
+        #         tmp=F.conv2d(input_i,c, d, self.stride,
+        #                     self.padding, self.dilation, self.groups)       #占用1048576
+        #
+        #
+        #
+        #         # out[:,i:i+1]=tmp    #不占用
+        #         out+=[tmp]
+        #     out=torch.cat(out,dim=1)
+        # except RuntimeError:
+        #     print()
+        #     out=1
+        return out
+
+
+
+
+class VGG(nn.Module):
+
+    def __init__(self, features, num_classes=1000, init_weights=True):
+        super(VGG, self).__init__()
+        self.features = features
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, num_classes),
+        )
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+
+def make_layers(cfg, batch_norm=False):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = conv2d_weighted_channel(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+
+cfg = {
+    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+def vgg11(pretrained=False, **kwargs):
+    """VGG 11-layer model (configuration "A")
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    if pretrained:
+        kwargs['init_weights'] = False
+    model = VGG(make_layers(cfg['A']), **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['vgg11']))
+    return model
+
+
+def vgg11_bn(pretrained=False, **kwargs):
+    """VGG 11-layer model (configuration "A") with batch normalization
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    if pretrained:
+        kwargs['init_weights'] = False
+    model = VGG(make_layers(cfg['A'], batch_norm=True), **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['vgg11_bn']))
+    return model
+
+
+def vgg13(pretrained=False, **kwargs):
+    """VGG 13-layer model (configuration "B")
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    if pretrained:
+        kwargs['init_weights'] = False
+    model = VGG(make_layers(cfg['B']), **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['vgg13']))
+    return model
+
+
+def vgg13_bn(pretrained=False, **kwargs):
+    """VGG 13-layer model (configuration "B") with batch normalization
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    if pretrained:
+        kwargs['init_weights'] = False
+    model = VGG(make_layers(cfg['B'], batch_norm=True), **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['vgg13_bn']))
+    return model
+
+
+def vgg16(pretrained=False, **kwargs):
+    """VGG 16-layer model (configuration "D")
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    if pretrained:
+        kwargs['init_weights'] = False
+    model = VGG(make_layers(cfg['D']), **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['vgg16']))
+    return model
+
+
+def vgg16_bn(pretrained=False, **kwargs):
+    """VGG 16-layer model (configuration "D") with batch normalization
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    if pretrained:
+        kwargs['init_weights'] = False
+    model = VGG(make_layers(cfg['D'], batch_norm=True), **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['vgg16_bn']))
+    return model
+
+
+def vgg19(pretrained=False, **kwargs):
+    """VGG 19-layer model (configuration "E")
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    if pretrained:
+        kwargs['init_weights'] = False
+    model = VGG(make_layers(cfg['E']), **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['vgg19']))
+    return model
+
+
+def vgg19_bn(pretrained=False, **kwargs):
+    """VGG 19-layer model (configuration 'E') with batch normalization
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    if pretrained:
+        kwargs['init_weights'] = False
+    model = VGG(make_layers(cfg['E'], batch_norm=True), **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['vgg19_bn']))
+    return model
