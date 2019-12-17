13f7acd3c31ea99670824a27ec6ca6ad7e6484ec
fang <569160112@qq.com>
2019-12-17 16:00:45 +0800

    code: 1.gcn for resnet:bottleneck

--- filter_characteristic/graph_convolutional_network.py
+++ filter_characteristic/graph_convolutional_network.py
@@ -28,15 +28,33 @@ class gcn(nn.Module):
             return self.forward_resnet(net,rounds)
 
     def forward_resnet(self,net,rounds):
-        first_conv=True
-        for name,mod in net.named_modules():
-            if first_conv and isinstance(mod,nn.Conv2d):
-                weight=conv_to_matrix(copy.deepcopy(mod))
-                information = weight.mean(dim=1).reshape([-1, 1])               # calculate the mean of current layer
-                first_conv = False
-            if isinstance(mod,resnet.Bottleneck):
-                information=self.aggregate_bottleneck(mod,information)          #conv at present is the last conv in front of bottleneck
+        net=copy.deepcopy(net)
+        while rounds>0:                                                                     #aggregate information in convs
+            rounds-=1
+            first_conv=True
+            for name,mod in net.named_modules():
+                if first_conv and isinstance(mod,nn.Conv2d):                                #first conv in the ResNet
+                    weight=conv_to_matrix(mod)
+                    information_at_last = weight.mean(dim=1).reshape([-1, 1])                 # calculate the mean of current layer
+                    first_conv = False
+                if isinstance(mod,resnet.Bottleneck):
+                    _,information_at_last=self.aggregate_bottleneck(mod,information_at_last)
 
+        weight_list=[]
+        for mod in net.modules():
+            if isinstance(mod,nn.Conv2d):
+                weight_list+=[conv_to_matrix(mod)]
+
+        gcn_feature_in = []
+        for i in range(len(weight_list)):
+            gcn_feature_in += [
+                pca(weight_list[i], dim=self.in_features)]  # reduce the dimension of all filters to same value
+
+        gcn_feature_out = []
+        for i in range(len(gcn_feature_in)):
+            gcn_feature_out += [self.network(gcn_feature_in[i])]  # foward propagate
+
+        return gcn_feature_out  # each object represents one conv
 
     def aggregate_bottleneck(self,bottleneck,information_in_front):
         '''
@@ -45,31 +63,26 @@ class gcn(nn.Module):
         :param conv_in_front: conv module in front of bottleneck
         :return:
         '''
-        information_downsample=None
         weight_dowmsample=None
         conv_list=[]
         for name,mod in bottleneck.named_modules():
 
             if isinstance(mod,nn.Conv2d):
                 if 'downsample' in name:
-                    weight_dowmsample = conv_to_matrix(copy.deepcopy(mod))
-                    information_downsample=weight_dowmsample.mean(dim=1).reshape([-1, 1])
-                    conv_downsample=copy.deepcopy(mod)
+                    weight_dowmsample = conv_to_matrix(mod)
                     continue
-                conv_list+=[copy.deepcopy(mod)]      #a list containing 2-d conv weight matrix, parameters in original nets will not be updated
+                conv_list+=[mod]      #a list containing 2-d conv weight matrix, parameters in original nets will not be updated
 
-        if weight_dowmsample is not None:
-            weight_dowmsample+=information_in_front.repeat(1, 1).view(-1)
-        self.aggregate_convs(conv_list,information_in_front)
-        weight_list=[]
-        for conv in conv_list:
-            weight_list+=[conv_to_matrix(conv)]
-        information_at_last=weight_list[-1].mean(dim=1).reshape([-1, 1])
-        if weight_dowmsample is not None:
+        _,information_at_last=self.aggregate_convs(conv_list,information_in_front)
+
+        # shortcut
+        if weight_dowmsample is not None:                                                       #with 1x1 conv
+            weight_dowmsample += information_in_front.repeat(1, 1).view(-1)
             information_at_last+=weight_dowmsample.mean(dim=1).reshape([-1, 1])
+        else:                                                                                   #without 1x1 conv
+            information_at_last+=information_in_front
 
-        print()
-        return 0
+        return conv_list,information_at_last
 
     def aggregate_convs(self,conv_list,information_in_front):
         '''
@@ -88,9 +101,10 @@ class gcn(nn.Module):
         for i in range(len(conv_list)):
             kernel_size=conv_list[i].kernel_size[0]*conv_list[i].kernel_size[1]
             mean = mean.repeat(1, kernel_size).view(-1)  # expand each value for 9 times.
-            # this implies that the default size of kernel is 3x3
             weight_list[i] += mean  # aggregate the mean from previous layer
             mean = weight_list[i].mean(dim=1).reshape([-1, 1])  # calculate the mean of current layer
+        information_at_last=mean
+        return conv_list,information_at_last                    #information from the last conv
 
     def forward_vgg(self, net, rounds):
         '''
@@ -99,20 +113,20 @@ class gcn(nn.Module):
         :param rounds:
         :return: extracted-features representing the cross layer relationship for each filter
         '''
-        weight_list=[]
-        for mod in net.modules():                                                   #mod is a copy
+        net=copy.deepcopy(net)
+        conv_list=[]
+        for mod in net.modules():
             if isinstance(mod,nn.Conv2d):
-                # weight_list+=[conv_to_matrix(mod)]                   #a list containing 2-d conv weight matrix
-                weight_list+=[conv_to_matrix(copy.deepcopy(mod))]      #a list containing 2-d conv weight matrix, parameters in original nets will not be updated
-
+                conv_list+=[mod]
         while rounds>0:
             rounds-=1
-            mean = torch.zeros(3, 1).to(weight_list[0].device)                      #initialize mean for first layer
-            for i in range(len(weight_list)):
-                mean=mean.repeat(1,9).view(-1)                                      #expand each value for 9 times.
-                                                                                    #this implies that the default size of kernel is 3x3
-                weight_list[i]+=mean                                                #aggregate the mean from previous layer
-                mean=weight_list[i].mean(dim=1).reshape([-1,1])                     #calculate the mean of current layer
+            mean = torch.zeros(3, 1).to(net.features[0].weight.device)                      #initialize mean for first layer
+            self.aggregate_convs(conv_list,mean)
+
+        weight_list=[]
+        for mod in net.modules():
+            if isinstance(mod,nn.Conv2d):
+                weight_list+=[conv_to_matrix(mod)]
 
         gcn_feature_in=[]
         for i in range(len(weight_list)):
@@ -133,6 +147,8 @@ def pca(tensor_2d,dim):
     :param dim:
     :return: tensor after dimension reduction,each row is a piece of data
     '''
+    if dim>tensor_2d.shape[1]:
+        raise Exception('Required dim is larger than feature len.(dim:{}>tensor_2d.shape[1]:{})'.format(dim,tensor_2d.shape[1]))
     u,s,v=torch.svd(tensor_2d)
     projection_matrix=v[:,:dim]
     return torch.matmul(tensor_2d,projection_matrix)
@@ -149,10 +165,16 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # net= vgg.vgg16_bn(pretrained=True).to(device)
-    net1= resnet_cifar.resnet56()
-    net2=resnet.resnet50()
+    net1= resnet_cifar.resnet56().to(device)
+    net2=resnet.resnet50().to(device)
+    net3=vgg.vgg16_bn().to(device)
+
 
     test=gcn(in_features=27,out_features=10).to(device)
+
+
+    test.forward(net=net3,net_name='vgg16_bn',dataset_name='imagenet',rounds=2)
+
     c=test.forward(net=net2,rounds=2,net_name='resnet50',dataset_name='imagenet')
     print()
     # for name, module in network.named_modules():
